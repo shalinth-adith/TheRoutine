bae71360a6fadcb5e1204714e86a066b67e545d9
shalinth-adith <shalinth.adith@outlook.com>
2025-07-10 22:13:37 +0530

    added logo , entry screen , delete option

---
+++ Routine/EntryView.swift
@@ -0,0 +1,18 @@
+//
+//  EntryView.swift
+//  Routine
+//
+//  Created by shalinth adithyan on 10/07/25.
+//
+
+import SwiftUI
+
+struct EntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EntryView()
+}

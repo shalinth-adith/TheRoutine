caabf3fbd4a7129e9d35b65d7e5dd9912dc4e2b7
shalinth-adith <shalinth.adith@outlook.com>
2025-07-10 17:05:20 +0530

    saved/load

--- Routine/Activities.swift
+++ Routine/Activities.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+@Observable
+class Activities{
+    var activities: [Activity] {
+        didSet{
+            if let encoded = try? JSONEncoder().encode(activities){
+                UserDefaults.standard.set(encoded, forKey: "activities")
+            }
+        }
+    }
+    init(){
+        if let saved = UserDefaults.standard.data(forKey: "activities"){
+            if let decoded = try? JSONDecoder().decode([Activity].self, from: saved){
+                self.activities = decoded
+                return
+            }
+        }
+        activities = []
+    }
+}
--- Routine/ContentView.swift
+++ Routine/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var data = Activities()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(data.activities){ activity in
+                NavigationLink{
+                    Text("Detail View")
+                }label:{
+                    HStack{
+                        Text(activity.title)
+                        
+                        Spacer()
+                        
+                        Text(String(activity.completionCount))
+                    }
+                }
+            }
+            .navigationTitle(Text("Routine"))
         }
-        .padding()
     }
 }
 

8dbdfd095666bf1124141e4c4a257c14f9e6b3e6
shalinth-adith <shalinth.adith@outlook.com>
2025-07-10 21:31:09 +0530

    adding colors , activity view

---
+++ ActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityView.swift
+//  Routine
+//
+//  Created by shalinth adithyan on 10/07/25.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityView()
+}

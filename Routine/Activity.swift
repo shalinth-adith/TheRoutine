f050c2ff7c01899a61d91d4f0a8ba69d1830146d
shalinth-adith <shalinth.adith@outlook.com>
2025-07-10 11:26:03 +0530

    create

--- Routine/Activity.swift
+++ Routine/Activity.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+
+struct Activity : Codable,Equatable,Identifiable {
+    var id = UUID()
+    var title : String
+    var description : String
+    var completionCount = 0
+    
+    static let example = Activity(title: "Example Activity", description: "This is an example activity")
+ }

db66784c91c70d4c34446f2f6cb26912be93f26e
shalinth-adith <shalinth.adith@outlook.com>
2025-07-10 21:31:37 +0530

    adding colors and  Activity view

--- Routine/ContentView.swift
+++ Routine/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var data = Activities()
+    @State private var addingNewActivity: Bool = false
     var body: some View {
         NavigationStack{
             List(data.activities){ activity in
@@ -21,10 +22,38 @@ struct ContentView: View {
                         Spacer()
                         
                         Text(String(activity.completionCount))
+                            .font(.caption.weight(.black))
+                            .padding(5)
+                            .frame(minWidth: 50)
+                            .background(color(for: activity))
+                            .foregroundStyle(.white)
+                            .clipShape(.capsule)
                     }
                 }
             }
             .navigationTitle(Text("Routine"))
+            .toolbar {
+                Button("Add new activity",systemImage: "plus"){
+                    addingNewActivity.toggle()
+                }
+            }
+            .sheet(isPresented: $addingNewActivity) {
+                AddActivity(data: data)
+            }
+        }
+    }
+    
+    func color(for activity : Activity) -> Color {
+        if activity.completionCount < 3 {
+             .red
+        }else if activity.completionCount < 10 {
+             .orange
+        }else if activity.completionCount < 20 {
+             .green
+        }else if activity.completionCount < 30 {
+             .blue
+        }else{
+             .indigo
         }
     }
 }
